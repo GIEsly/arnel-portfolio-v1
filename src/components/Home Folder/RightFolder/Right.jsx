6f7f0c3671851bae27c7dffa397b8100c231ea1b
GIEsly <kyzerushtine@gmail.com>
2026-08-23 03:47:56 +0400

    updated some animation

--- src/components/Home Folder/RightFolder/Right.jsx
+++ src/components/Home Folder/RightFolder/Right.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import pics from '../../assets/images/me/arnel-1.png'
-import code from '../../assets/icons/code.png'
+import signature from '../../assets/icons/logoFolder/signatureName/arnelLogoFont.png'
 import './right.scss'
 
 const Right = () => {
   return (
      <div className="right">
-        <img src={pics} alt="my iamge"/>
+        <img src={pics} alt="my iamge" className='mainImg'/>
+        
+        <div className="sig">
+          <img src={signature} alt="name signature" className='signature'/>
+        </div>
     </div>
   )
 }
